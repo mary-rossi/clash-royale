667b548239efaf561dc1118ce7635e8db4754bfc
Mary Rossi <maryrossi106@gmail.com>
2022-09-16 01:46:44 -0500

    finished

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { response } = require("express");
 const PORT = process.env.PORT;
 const allCards = require("./models/data");
 const Card = require("./models/deck");
+const Deck = require("./models/deck");
 
 // Connect to MongoDB Atlas
 mongoose.connect(process.env.DATABASE_URL, {
@@ -21,8 +22,13 @@ db.on("connected", () => console.log("Mongo is connected."));
 db.on("disconnected", () => console.log("Mongo is disconnected."));
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); 
+
+// Commented out this line because it was causing our req.body to be empty on POST requests
+// app.use(express.urlencoded({ extended: true }));
+
+// Using express.json() instead -- https://stackoverflow.com/questions/24543847/req-body-empty-on-posts
+app.use(express.json());
 
 /************
 Page templates
@@ -35,7 +41,7 @@ app.get("/", (req, res) => {
 
 // Render Saved Decks Page
 app.get("/saved-decks", (req, res) => {
-    // res.render("saved-decks.ejs")
+    res.render("savedDecks.ejs")
 });
 
 
@@ -54,25 +60,56 @@ app.get("/cards/:limit", async (req, res) => {
     res.send(randomCards);
 });
 
-// GET / Read
+// GET / Read / One deck
 app.get("/deck", async (req, res) => {
     // read deck from database
     res.send(deck);
 });
 
-// POST / Create / Save
-app.post("/deck/save", async () => {
-    // save deck to database
+// GET / Read / All decks
+app.get("/decks/all", async (req, res) => {
+    const allDecks = await Deck.find();
+    res.send(allDecks)
+})
+
+// POST / Create / Save - referred jenniferbland.com mongodb
+app.post("/deck/save", (req, res) => {
+    const myDeck = new Deck(req.body);
+
+    myDeck.save()
+        .then(deck => {
+            res.send(deck)
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database:", err)
+        })
 })
 
 // PATCH / Update / Edit
-app.patch("/deck/edit", async () => {
+app.patch("/deck/edit", async (req, res) => {
     // edit deck that's in database
+    const deckToEdit = new Deck(req.body.deck);
+    deckToEdit.updateOne({ name: req.body.nameUpdate })
+        .then(response => {
+            res.send({
+                response: response,
+                newDeck: {
+                    ...req.body.deck,
+                    name: req.body.nameUpdate
+                }
+            });
+        })
+        .catch(err => {
+            res.send.status(400).send("Failed to update:", err)
+        });
 })
 
 // DELETE / Remove
-app.delete("/deck/delete", async () => {
+app.delete("/deck/delete/:id", (req, res) => {
     // delete deck from database
+    Deck.deleteOne({ _id: req.params.id })
+        .then(id => res.send(id))
+        .catch(err => res.status(400).send("Failed to delete:", err))
 })
 
 
